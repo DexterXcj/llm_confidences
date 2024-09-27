f0a61dcde3e975174391f835152b03828472e726
Dexter Xu <82686252+DexterXcj@users.noreply.github.com>
2024-09-27 12:00:05 +0800

    Update params.py
    change to Non-binary individual

--- src/params.py
+++ src/params.py
@@ -1,6 +1,6 @@
 # Define parameters
 ages = ["Young Adult", "Middle-aged", "Senior"]
-genders = ["Male", "Female", "Non-binary gender"]
+genders = ["Male", "Female", "Non-binary individual"]
 races = ["White", "Black", "Asian", "Hispanic"]
 
 experiment_types = [
